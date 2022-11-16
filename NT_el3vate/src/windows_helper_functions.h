8eef68f7ea41fba4695877928bdc07c833660de4
vnescape <vnescape@protonmail.com>
2022-11-16 13:14:42 +0100

    Add _CM_PARTIAL_RESOURCE_DESCRIPTOR and _CM_PARTIAL_RESOURCE_LIST struct

--- NT_el3vate/src/windows_helper_functions.h
+++ NT_el3vate/src/windows_helper_functions.h
@@ -2,8 +2,116 @@
 #include <Windows.h>
 #include <winternl.h>
 #include <stdio.h>
+#include <ntdef.h>
 
-#define SystemModuleInformation (SYSTEM_INFORMATION_CLASS)0x0B
+#define SystemModuleInformation (SYSTEM_INFORMATION_CLASS)0x0B#
+
+//Source: https://learn.microsoft.com/en-us/windows-hardware/drivers/ddi/wdm/ns-wdm-_cm_partial_resource_descriptor
+typedef struct _CM_PARTIAL_RESOURCE_DESCRIPTOR {
+    UCHAR  Type;
+    UCHAR  ShareDisposition;
+    USHORT Flags;
+    union {
+        struct {
+            PHYSICAL_ADDRESS Start;
+            ULONG            Length;
+        } Generic;
+        struct {
+            PHYSICAL_ADDRESS Start;
+            ULONG            Length;
+        } Port;
+        struct {
+#if ...
+            USHORT    Level;
+            USHORT    Group;
+#else
+            ULONG     Level;
+#endif
+            ULONG     Vector;
+            KAFFINITY Affinity;
+        } Interrupt;
+        struct {
+            union {
+                struct {
+                    USHORT    Group;
+                    USHORT    Reserved;
+                    USHORT    MessageCount;
+                    ULONG     Vector;
+                    KAFFINITY Affinity;
+                } Raw;
+                struct {
+#if ...
+                    USHORT    Level;
+                    USHORT    Group;
+#else
+                    ULONG     Level;
+#endif
+                    ULONG     Vector;
+                    KAFFINITY Affinity;
+                } Translated;
+            } DUMMYUNIONNAME;
+        } MessageInterrupt;
+        struct {
+            PHYSICAL_ADDRESS Start;
+            ULONG            Length;
+        } Memory;
+        struct {
+            ULONG Channel;
+            ULONG Port;
+            ULONG Reserved1;
+        } Dma;
+        struct {
+            ULONG Channel;
+            ULONG RequestLine;
+            UCHAR TransferWidth;
+            UCHAR Reserved1;
+            UCHAR Reserved2;
+            UCHAR Reserved3;
+        } DmaV3;
+        struct {
+            ULONG Data[3];
+        } DevicePrivate;
+        struct {
+            ULONG Start;
+            ULONG Length;
+            ULONG Reserved;
+        } BusNumber;
+        struct {
+            ULONG DataSize;
+            ULONG Reserved1;
+            ULONG Reserved2;
+        } DeviceSpecificData;
+        struct {
+            PHYSICAL_ADDRESS Start;
+            ULONG            Length40;
+        } Memory40;
+        struct {
+            PHYSICAL_ADDRESS Start;
+            ULONG            Length48;
+        } Memory48;
+        struct {
+            PHYSICAL_ADDRESS Start;
+            ULONG            Length64;
+        } Memory64;
+        struct {
+            UCHAR Class;
+            UCHAR Type;
+            UCHAR Reserved1;
+            UCHAR Reserved2;
+            ULONG IdLowPart;
+            ULONG IdHighPart;
+        } Connection;
+    } u;
+} CM_PARTIAL_RESOURCE_DESCRIPTOR, * PCM_PARTIAL_RESOURCE_DESCRIPTOR;
+
+//Source: https://learn.microsoft.com/en-us/windows-hardware/drivers/ddi/wdm/ns-wdm-_cm_partial_resource_list
+typedef struct _CM_PARTIAL_RESOURCE_LIST {
+	USHORT                         Version;
+	USHORT                         Revision;
+	ULONG                          Count;
+	CM_PARTIAL_RESOURCE_DESCRIPTOR PartialDescriptors[1];
+} CM_PARTIAL_RESOURCE_LIST, * PCM_PARTIAL_RESOURCE_LIST;
+
 
 typedef struct struct_buffer
 {
